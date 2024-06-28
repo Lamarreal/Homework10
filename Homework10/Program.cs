713bff152277b16f0f2cc3c74a83b2c44a5c2257
maksb <maksb@MAKSPC>
2024-06-28 10:21:26 +0300

    Добавьте файлы проекта.

---
+++ Homework10/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+
+const string phoneNumberPattern = @"^[+]?\d{12}$";
+const string emailPattern = @"^[a-z]+\d*[@][a-z]+[.][a-z]{2,5}$";
+const string fioPattern = @"^([A-Z]{1}[a-z]{1,19}\s){2}[A-Z]{1}[a-z]{1,19}$";
+
+string phoneNumber = "+380980592066";
+string TestEmail = "pupok@gmail.com";
+string Fio = "Vasya Vasya Vasya";
+
+bool IsPhoneNumber(string PhoneNumber)
+{
+    if (PhoneNumber != null)
+        return Regex.IsMatch(PhoneNumber, phoneNumberPattern);
+    return false;
+}
+
+bool IsEmail(string Email)
+{
+    if (Email != null)
+        return Regex.IsMatch(Email, emailPattern);
+    return false;
+}
+
+bool ValidateFio(string Fio)
+{
+    if (Fio != null)
+        return Regex.IsMatch(Fio, fioPattern);
+    return false;
+}
+
+
+
+Console.WriteLine(IsPhoneNumber(phoneNumber));
+
+Console.WriteLine(IsEmail(TestEmail));
+
+Console.WriteLine(ValidateFio(Fio));
